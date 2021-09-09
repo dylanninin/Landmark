666bf1a402251116c06b8bba9116ea45a25b84be
Dylan <dylanninin@gmail.com>
2021-09-09 22:46:33 +0800

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by dylanninin on 2021/9/9.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
