62666c3fb3db5d87eeae1f56a2ef1f56bc2aa346
Dylan <dylanninin@gmail.com>
2021-09-09 23:08:39 +0800

    feat: implement content view

--- Landmark/ContentView.swift
+++ Landmark/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack (alignment: .leading) {
+            Text("圆明园").font(.title)
+            HStack {
+                Text("名胜古迹").font(.subheadline)
+                Spacer()
+                Text("北京").font(.subheadline)
+            }
+        }.padding()
     }
 }
 
