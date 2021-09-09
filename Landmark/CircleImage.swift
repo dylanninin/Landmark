be0376e46407e0d767137c88c44690b5506483b2
Dylan <dylanninin@gmail.com>
2021-09-09 23:27:38 +0800

    feat: add circle image

---
+++ Landmark/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmark
+//
+//  Created by dylanninin on 2021/9/9.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+
+        Image("yml").clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
